16708c3171796490bcdfc381db39d92e46ee649d
it21_maismail <it21_maismail@bs1-kempten.de>
2024-02-02 11:14:58 +0100

    first api request

--- src/components/search.vue
+++ src/components/search.vue
@@ -10,39 +10,48 @@
   
   <script>
   import axios from 'axios';
-  const API_KEY = "RGAPI-0c3f740e-039e-481f-975d-567e70b7b028";
+  
+  const API_KEY = "RGAPI-ca3dd482-441c-44f8-b78a-841876e62ed2";
+  
   export default {
     data() {
       return {
-        searchText: '', // Initialize searchText in data
+        searchText: '',
       };
     },
     methods: {
       searchForPlayer() {
-        // Access the search text using this.searchText
-        console.log('Searching for player:', this.searchText);
+        // Set up correct API call
+        const APICallString = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${this.searchText}?api_key=${API_KEY}`;
         
-        this.searchText = '';
+        // Handle API call
+        axios.get(APICallString)
+          .then(response => {
+            // Success - Handle the response as needed
+            console.log(response.data);
+          })
+          .catch(error => {
+            // Error - Handle the error
+            console.error(error);
+          });
       }
     }
   }
   </script>
   
-
-  
-<style lang="scss">
-.search{
-    .container{
-        h5{
-            font-size: 22px;
-        }
-        input{
-            padding: 8px;
-        }
-        button{
-            font-size: 18px;
-        }
+  <style lang="scss">
+  .search {
+    .container {
+      h5 {
+        font-size: 22px;
+      }
+      input {
+        padding: 8px;
+      }
+      button {
+        font-size: 18px;
+      }
     }
-}
-</style>
+  }
+  </style>
   
